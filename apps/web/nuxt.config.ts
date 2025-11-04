a008efef1ccbdf544474c58131795ee5a82d4347
Mjnhdi35 <mjnhdi35@gmail.com>
2025-11-05 01:36:57 +0700

    update web

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: false },
   ssr: false,
 
@@ -8,5 +9,7 @@ export default defineNuxtConfig({
     typeCheck: true,
   },
 
-  modules: ['@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
+
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon', '@nuxt/fonts'],
 });
